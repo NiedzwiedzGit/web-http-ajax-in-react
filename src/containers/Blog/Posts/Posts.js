1e5b3c066f687d9c3b43752d99559e80c5afd76d
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-04-11 16:48:02 +0200

    Navigating Programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Post from '../../../components/Post/Post';
 import axios from '../../../axios';
 import './Posts.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 class Posts extends Component {
     state = {
         posts: [],
@@ -28,20 +28,22 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
+        this.props.history.push({ pathname: '/' + id });
+        //this.props.history.push({'/' + id });
     }
     render() {
         let posts = <p style={{ textAlign: 'center' }}>Somthing went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    < Link to={'/' + post.id} key={post.id}>
-                        <Post
-
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link >);
+                    //< Link to={'/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                    // </Link >
+                );
             }
             );
         }
